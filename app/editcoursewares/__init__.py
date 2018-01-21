4fa38380e87d54245f7bbb2bb06a7833312cae7d
MatthiasHua <377970563@qq.com>
2018-01-21 20:36:32 +0800

    注释

--- app/editcoursewares/__init__.py
+++ app/editcoursewares/__init__.py
@@ -10,9 +10,13 @@ editcoursewares = Blueprint('editcoursewares', __name__,  template_folder='templ
 
 @editcoursewares.route('/id/<int:id>/chapter/<int:chapter>', methods=['POST', 'GET'])
 def editbychapter(id, chapter):
+    #当前页所指的课程
     currentcourses = Class.query.filter_by(id = id).first()
+    #当前课程的所有章节
     chapters = currentcourses.chapter.all()
+    #当前页所指的章节
     currentchapter = currentcourses.chapter.filter_by(index = chapter).first()
+    #返回页面
     if request.method == 'GET':
         lessons = currentchapter.lesson.all()
         return render_template("EditbyChapter.html",\
@@ -23,22 +27,38 @@ def editbychapter(id, chapter):
         id = id,\
         introduction = currentchapter.introduction,\
         chapter = chapter - 1)
+    #接收到表单
+    #这一页三个按钮
+    #修改章节导引
+    #修改章节名称
+    #添加新课件
     if request.method == 'POST':
         print(request.form)
+        #这里做的时候还只有一个按钮所以没判断是那个按钮，在post表单里可以加一个变量来判断是那个按钮
+        #不为空
         if request.form.get('introduction', '') != '':
+            #修改数据
             currentchapter.introduction = request.form['introduction']
+            #提交到数据库
             db.session.commit()
+            #返回运行成功
             return 'Done'
+        #修改失败
         return '233'
 
 
 @editcoursewares.route('/id/<int:id>/chapter/<int:chapter>/lesson/<int:lesson>', methods=['POST', 'GET'])
 def editbylesson(id, chapter, lesson):
+    #当前页所指的课程
     currentcourses = Class.query.filter_by(id = id).first()
+    #当前课程的所有章节
     chapters = currentcourses.chapter.all()
+    #当前页所指的章节
     currentchapter = currentcourses.chapter.filter_by(index = chapter).first()
     if request.method == 'GET':
+        #当前章节的所有Lesson
         lessons = currentchapter.lesson.all()
+        #当前课的内容
         body = currentchapter.lesson.filter_by(index = lesson).first().body
         return render_template("EditbyLesson.html",\
         chapters = chapters,\
@@ -50,11 +70,19 @@ def editbylesson(id, chapter, lesson):
         currentcourses = currentcourses,\
         chapter = chapter - 1,\
         lesson = lesson)
+    #接收到表单
+    #这一页两个个按钮
+    #修改课件内容
+    #修改课件标题
     if request.method == 'POST':
         print(request.form)
+        #不为空
         if request.form.get('body', '') != '':
             currentlesson = currentchapter.lesson.filter_by(index = lesson).first()
+            #修改数据
             currentlesson.body = request.form['body']
             db.session.commit()
+            #返回运行成功
             return 'Done'
+        #修改失败
         return '233'
