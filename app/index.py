5c0f4d98201092475a3b52ea1be9c15f6671c1e0
MatthiasHua <377970563@qq.com>
2018-02-19 13:03:00 +0800

    测试页面

--- app/index.py
+++ app/index.py
@@ -4,12 +4,20 @@ from app import app
 from flask import render_template, session
 
 @app.route('/')
-@app.route('/index')
+@app.route('/index', methods=['GET'])
 def index():
 	return render_template('index.html',\
 	role = session.get('role', ''),\
 	username = session.get('username', ''))
 
-@app.route('/404')
+@app.route('/404', methods=['GET'])
 def error_404():
+	return render_template('404.html')
+
+@app.route('/test', methods=['GET'])
+def test():
+	return render_template('test.html')
+
+@app.route('/feedback', methods=['POST'])
+def feedback():
 	return render_template('404.html')
