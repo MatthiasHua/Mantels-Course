0dda1f800583e2c44c4cbe9e34eb6886b9bec8fe
MatthiasHua <377970563@qq.com>
2018-05-14 20:35:41 +0800

    编码

--- app/modules/qrcode.py
+++ app/modules/qrcode.py
@@ -10,18 +10,7 @@ def get_qr_string(data):
 	qr.add_data(data)
 	#print(qr.get_matrix())
 	s = ""
-	t = 0
-	tmp = 0
 	for i in qr.get_matrix():
 		for j in i:
-			t = (t + 1) % 8
-			tmp = tmp * 2 + (1 if (j) else 0);
-			if t == 0:
-				#print(tmp)
-				s += chr(tmp)
-				tmp = 0
-	for i in range(8 - t):
-		tmp *= 2
-	s += chr(tmp)
-	#print(s)
+			s += "1" if (j) else "0";
 	return s;
